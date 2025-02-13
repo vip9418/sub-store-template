a803c037b17cdfb7c96a61e78a107aea8307856d
vip9418 <vip9418@outlook.com>
2025-02-13 19:17:29 +0800

    Update sing-box_base.js

--- sing-box_base.js
+++ sing-box_base.js
@@ -1,53 +1,68 @@
-const { type, name } = $arguments
-const compatible_outbound = {
+const { type, name } = $arguments;
+const compatibleOutbound = {
   tag: 'COMPATIBLE',
   type: 'direct',
-}
+};
 
-let compatible
-let config = JSON.parse($files[0])
+let config = JSON.parse($files[0]);
 let proxies = await produceArtifact({
   name,
   type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
   platform: 'sing-box',
   produceType: 'internal',
-})
+});
 
-config.outbounds.push(...proxies)
+config.outbounds.push(...proxies);
 
 config.outbounds.map(i => {
   if (['all', 'all-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies))
+    i.outbounds.push(...getTags(proxies));
   }
   if (['hk', 'hk-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
+    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i));
   }
   if (['tw', 'tw-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
+    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i));
   }
   if (['jp', 'jp-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i));
+  }
+    if (['kr', 'kr-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /韩|kr|korea|🇰🇷/i));
+  }
+    if (['uk', 'uk-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /英|uk|unitedkingdom|🇬🇧/i));
+  }
+    if (['de', 'de-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /德|de|germany|🇩🇪/i));
+  }
+    if (['fr', 'fr-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /法|fr|france|🇫🇷/i));
+  }
+   if (['nl', 'nl-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /荷|nl|netherlands|🇳🇱/i));
   }
   if (['sg', 'sg-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i));
   }
   if (['us', 'us-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
-  }
-})
-
-config.outbounds.forEach(outbound => {
-  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
-    if (!compatible) {
-      config.outbounds.push(compatible_outbound)
-      compatible = true
-    }
-    outbound.outbounds.push(compatible_outbound.tag);
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i));
   }
 });
 
-$content = JSON.stringify(config, null, 2)
+let compatibleAdded = false;
+config.outbounds.forEach(outbound => {
+    if (outbound.tag === "proxy" && Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+        if (!config.outbounds.some(o => o.tag === "compatible")) {
+            config.outbounds.push(compatibleOutbound);
+        }
+        outbound.outbounds.push(compatibleOutbound.tag);
+        compatibleAdded = true; 
+    }
+});
+
+$content = JSON.stringify(config, null, 2);
 
 function getTags(proxies, regex) {
-  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag);
 }
